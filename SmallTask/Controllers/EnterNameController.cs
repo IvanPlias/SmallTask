751b91b3229e607f5146203abea45063f2e4e798
Ivan Plias <sana22_pii@student.ztu.edu.ua>
2023-03-27 17:13:10 +0300

    Task Done

---
+++ SmallTask/Controllers/EnterNameController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmallTask.Models;
+
+namespace SmallTask.Controllers
+{
+    public class EnterNameController : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            return View(user);
+        }
+    }
+}
